d65808005dd5bf8d6bc049af7c653eb2670bfd69
egetuncel <64143418+egetuncel@users.noreply.github.com>
2022-05-08 02:02:49 +0300

    Fetch Functions
    Creation of fetch functions.

--- App.js
+++ App.js
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,75 +7,69 @@ import {
   Text,
   useColorScheme,
   View,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const fetchUsers = async () => {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await resp.json();
+    setData(data);
+    setLoading(false);
   };
 
+  const fetchUsersById = async () => {
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const data = await resp.json();
+    
+  };
+
+  const fetchPosts = async () => {
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await resp.json();
+    
+  };
+
+  const fetchPostsById = async () => {
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const data = await resp.json();
+    
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
+    <SafeAreaView>
+
+      <ScrollView>
+
+
+        <View style={styles.sectionContainer}> 
+        {loading ? <ActivityIndicator /> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.name}</Text>
+            )}
+
+          />
+        )}
+
         </View>
+
+
+
+
       </ScrollView>
     </SafeAreaView>
   );
@@ -95,18 +80,7 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
